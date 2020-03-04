1c296788b6b298a9c194891cabb30a8f4de88597
bochoven <arjen@bochoven.net>
2020-03-04 21:54:43 +0100

    Force text renderen in datatables

--- views/backup2go_listing.php
+++ views/backup2go_listing.php
@@ -42,7 +42,7 @@
             columnDefs = [{ visible: false, targets: hideThese }]; //Column Definitions
 
         $('.table th').map(function(){
-            columnDefs.push({name: $(this).data('colname'), targets: col});
+            columnDefs.push({name: $(this).data('colname'), targets: col, render: $.fn.dataTable.render.text()});
             if($(this).data('sort')){
               mySort.push([col, $(this).data('sort')])
             }
